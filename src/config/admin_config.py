c915dc4dae98d56242240fe49d5e8a0d700ba96f
JuniorD-Isael <isaeldjunior@gmail.com>
2024-08-08 23:58:15 -0300

    Cria o arquivo de confiração do adm

---
+++ src/config/admin_config.py
@@ -0,0 +1,7 @@
+class Admin:
+    """Classe de configuração para o administrador"""
+    ADMIN_PASSWORD = "@admin1234"
+
+    def __init__(self) -> None:
+        """Inicializa a configuração do administrador"""
+        pass
