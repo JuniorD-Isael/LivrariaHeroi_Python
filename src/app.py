c88018e8fea14b48a4fb2b35995745c3935b5909
JuniorD-Isael <isaeldjunior@gmail.com>
2024-08-10 22:58:30 -0300

    Criada a estrutura do admin_menu e app.py pra iniciar

--- src/app.py
+++ src/app.py
@@ -1,29 +1,5 @@
-OPCAO_INVALIDA = "\nOpção invalida! \n"
+from src.menu.menu_inicial import menu
 
-
-def menu():
-    input_usuario = -1
-    while input_usuario != 0:
-        try:
-            escolha = input("Escolha uma das opções a seguir \n"
-                            "1 - Cadastrar usuario \n"
-                            "2- Fazer login \n"
-                            "0 - Sair \n"
-                            ">>> ")
-            input_usuario = int(escolha)
-
-            match input_usuario:
-                case 0:
-                    break
-                case 1:
-                    print("Cadasto de usuário")
-                case 2:
-                    print("Fazer login")
-                case _:
-                    print(OPCAO_INVALIDA)
-        except ValueError:
-            print(OPCAO_INVALIDA)
-
-
-if __name__ == '__main__':
-    menu()
+print("Livraria Heroi")
+# popular_db()
+menu()
